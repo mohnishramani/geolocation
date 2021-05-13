a627f9b01727f5b0c3d8688c8529dec762a11355
Mohnish ramani 2 <mohnish@nglfoods.com>
2021-05-14 02:20:42 +0400

    geolocation

--- Index.js
+++ Index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const Datastore = require('nedb');
 
 const app = express();
-app.listen(5000, () => console.log('listening at 5000'));
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Starting server at ${port}`);
+});
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
