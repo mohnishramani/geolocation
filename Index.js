37104b59172a22841e45079baac2ef7c1e810af8
Mohnish ramani 2 <mohnish@nglfoods.com>
2021-05-15 23:34:24 +0400

    changed the PORT settin

--- Index.js
+++ Index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const Datastore = require('nedb');
 const fetch = require('node-fetch');
 const app = express();
-const port = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Starting server at ${PORT}`);
+    console.log(`Our app is running on port ${ PORT }`);
 });
+
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
