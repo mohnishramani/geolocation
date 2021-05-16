3a96705a8e40d70bd6c38ec0b1d11aa0ac88ec50
mohnish <mohnish@nglfoods.com>
2021-05-16 17:38:35 +0400

    add wildcard route

--- Index.js
+++ Index.js
@@ -5,7 +5,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
+    console.log(`Our app is running on port ${PORT}`);
 });
 
 app.use(express.static('public'));
@@ -13,19 +13,24 @@ app.use(express.json({ limit: '1mb' }));
 
 const database = new Datastore('datbase.db');
 database.loadDatabase();
-app.get('/api', (request, response)=>  {
-database.find({}, (err,data)=> { if (err) {
-response.end();
-return;
-}
-response.json(data);
-});
+app.get('/api', (request, response) => {
+    database.find({}, (err, data) => {
+        if (err) {
+            response.end();
+            return;
+        }
+        response.json(data);
+    });
 });
 
-app.post ('/api', (request, response)=>  {
-const data = request.body;
-const timestamp = Date.now();
-data.timestamp=timestamp;
-database.insert(data);
-response.json(data);
+app.post('/api', (request, response) => {
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+    response.json(data);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
 });
