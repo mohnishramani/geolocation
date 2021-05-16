907b3ba2d85bc32ddf2858932399850c39612730
mohnish <mohnish@nglfoods.com>
2021-05-16 14:42:05 +0400

    refresh

--- Index.js
+++ Index.js
@@ -3,7 +3,7 @@ const Datastore = require('nedb');
 const fetch = require('node-fetch');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
